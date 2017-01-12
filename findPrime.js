732c8ededde47c1d486b5c294d012248dea09e53
Steven Huggard <stevenUS@users.noreply.github.com>
2017-01-11 19:34:08 -0800

    find Prime Added w/ correct spacing

--- findPrime.js
+++ findPrime.js
@@ -1,14 +1,14 @@
 function findPrime(n){
     var primeArr;
-    if(n === 2){
+    if (n === 2) {
         primeArr = [2];
-    }else if(n === 3){
+    }else if (n === 3) {
         primeArr = [2, 3];
-    }else{
+    }else {
         var primeArr = [2, 3];
         var flag = false;
-        for(var x = 4; x <= n; x++){
-            for(var i = 2; i <= x; i++){
+        for (var x = 4; x <= n; x++) {
+            for (var i = 2; i <= x; i++) {
                 if (x != i && x % i === 0) {
                     flag = true;
                 }
